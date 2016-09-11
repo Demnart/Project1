af96636054ee7fb4e8b496ed8cd72cce77a125cd
Demnart <insidia93@gmail.com>
2016-09-11 20:45:24 +0300

    Добавленны переменные , конструктор, и метод вывода на экран

--- src/Dog.java
+++ src/Dog.java
@@ -2,5 +2,14 @@
  * Created by Артем on 11.09.2016.
  */
 public class Dog {
+    int age;
+    String name;
 
+    public Dog(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    void about(){
+        System.out.printf("Имя %d \t,Возраст %s",age,name);
+    }
 }
