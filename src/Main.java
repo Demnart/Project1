d9200a133a2d818cdbc135e083015245c0144c86
Demnart <insidia93@gmail.com>
2016-09-11 20:50:12 +0300

    Добавлен обьект собака,вызов метода собаки и кошки

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat(5, "Murz", "Red");
+        Dog dog = new Dog(5, "Bagira");
+        cat.about();
+        dog.about();
     }
 }
