d2477022cab2789fab1b218a3c8ec3d06c60ddd4
Demnart <insidia93@gmail.com>
2016-09-11 20:40:43 +0300

    Создан класс кошка,конструктор, а так же метод вывода на экран

---
+++ src/Cat.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Артем on 11.09.2016.
+ */
+public class Cat {
+    int age;
+    String name;
+    String color;
+
+    public Cat(int age, String name, String color) {
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+    void about(){
+        System.out.printf("Возраст: %d,Имя: %s,Цвет: %s",age,name,color);
+    }
+}
