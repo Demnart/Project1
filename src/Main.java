2b1b01ea92e75431d4f621593e6159fe823c1f4f
Demnart <insidia93@gmail.com>
2016-09-11 20:30:34 +0300

    Добавили 5 souta

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,10 @@ public class Main {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
