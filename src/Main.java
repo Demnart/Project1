e5f2d45ed816ed7ac03981d9ceedbdeeb5b05bfc
Demnart <insidia93@gmail.com>
2016-09-11 20:34:20 +0300

    Очищенно

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("how are you");
-        System.out.println("how are you");
-        System.out.println("how are you");
-        System.out.println("how are you");
-        System.out.println("how are you");
-        System.out.println("how are you");
-        System.out.println("how are you");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+
     }
 }
